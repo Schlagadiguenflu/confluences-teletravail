b198d05db6b2db07c694c9b8d6d4ab744704edb7
Tim <tim.allemann@outlook.com>
2020-10-11 14:21:49 +0200

    samesite cookies on mvc

--- src/mvc/Startup.cs
+++ src/mvc/Startup.cs
@@ -19,6 +19,7 @@ using System.Net.Http;
 using IdentityModel.Client;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using System.Threading.Tasks;
+using mvc.Utility;
 
 namespace mvc
 {
@@ -46,60 +47,7 @@ namespace mvc
                 options.DefaultScheme = "Cookies";
                 options.DefaultChallengeScheme = "oidc";
             })
-             .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options =>
-             {
-                 options.Events = new CookieAuthenticationEvents
-                 {
-                     // this event is fired everytime the cookie has been validated by the cookie middleware,
-                     // so basically during every authenticated request
-                     // the decryption of the cookie has already happened so we have access to the user claims
-                     // and cookie properties - expiration, etc..
-                     OnValidatePrincipal = async x =>
-                     {
-                         // since our cookie lifetime is based on the access token one,
-                         // check if we're more than halfway of the cookie lifetime
-                         var now = DateTimeOffset.UtcNow;
-                         var timeElapsed = now.Subtract(x.Properties.IssuedUtc.Value);
-                         var timeRemaining = x.Properties.ExpiresUtc.Value.Subtract(now);
-
-                         if (timeElapsed > timeRemaining)
-                         {
-                             var identity = (ClaimsIdentity)x.Principal.Identity;
-                             var accessTokenClaim = identity.FindFirst("access_token");
-                             var refreshTokenClaim = identity.FindFirst("refresh_token");
-
-                             // if we have to refresh, grab the refresh token from the claims, and request
-                             // new access token and refresh token
-                             var refreshToken = refreshTokenClaim.Value;
-                             var response = await new HttpClient().RequestRefreshTokenAsync(new RefreshTokenRequest
-                             {
-                                 Address = Configuration["URLIdentityServer4"] + "connect/token",
-                                 ClientId = "mvc",
-                                 ClientSecret = "secret",
-                                 RefreshToken = refreshToken
-                             });
-
-                             if (!response.IsError)
-                             {
-                                 // everything went right, remove old tokens and add new ones
-                                 identity.RemoveClaim(accessTokenClaim);
-                                 identity.RemoveClaim(refreshTokenClaim);
-
-                                 identity.AddClaims(new[]
-                                 {
-                                        new Claim("access_token", response.AccessToken),
-                                        new Claim("refresh_token", response.RefreshToken)
-                                    });
-
-                                 // indicate to the cookie middleware to renew the session cookie
-                                 // the new lifetime will be the same as the old one, so the alignment
-                                 // between cookie and access token is preserved
-                                 x.ShouldRenew = true;
-                             }
-                         }
-                     }
-                 };
-             })
+             .AddCookie("Cookies")
              .AddOpenIdConnect("oidc", options =>
               {
                   options.SignInScheme = "Cookies";
@@ -125,33 +73,7 @@ namespace mvc
                   options.TokenValidationParameters.RoleClaimType = "role";
 
 
-                  options.Events = new OpenIdConnectEvents
-                  {
-
-                      // that event is called after the OIDC middleware received the auhorisation code,
-                      // redeemed it for an access token and a refresh token,
-                      // and validated the identity token
-                      OnTokenValidated = x =>
-                          {
-                          // store both access and refresh token in the claims - hence in the cookie
-                          var identity = (ClaimsIdentity)x.Principal.Identity;
-                              identity.AddClaims(new[]
-                              {
-                                new Claim("access_token", x.TokenEndpointResponse.AccessToken),
-                                new Claim("refresh_token", x.TokenEndpointResponse.RefreshToken)
-                                });
-
-                          // so that we don't issue a session cookie but one with a fixed expiration
-                          x.Properties.IsPersistent = true;
-
-                          // align expiration of the cookie with expiration of the
-                          // access token
-                          var accessToken = new JwtSecurityToken(x.TokenEndpointResponse.AccessToken);
-                              x.Properties.ExpiresUtc = accessToken.ValidTo;
-
-                              return Task.CompletedTask;
-                          }
-                  };
+                  
               });
 
             //services.AddSingleton<Microsoft.AspNetCore.Authentication.IClaimsTransformation, KarekeClaimsTransformer>();
@@ -189,11 +111,17 @@ namespace mvc
                     opts.SupportedUICultures = supportedCultures;
                             });
             //
+            services.ConfigureNonBreakingSameSiteCookies();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // Add this before any other middleware that might write cookies
+            app.UseCookiePolicy();
+
+            // This will write cookies, so make sure it's after the cookie policy
+            app.UseAuthentication();
 
             var fordwardedHeaderOptions = new ForwardedHeadersOptions
             {
